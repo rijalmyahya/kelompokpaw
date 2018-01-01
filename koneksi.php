5ac11d5d57cfe26141f0b270d7e409fcf30c7fcb
luthfimuh <luthfimadz13@gmail.com>
2018-01-01 17:32:14 +0700

    tryagain4

--- koneksi.php
+++ koneksi.php
@@ -1,3 +1,9 @@
 <?php 
-$conn = mysqli_connect("localhost","root","","market");
+$db_server = "localhost";
+$db_user = "root";
+$db_pwd = "";
+$dbname = "market";
+
+#connect to database
+$conn = mysqli_connect($db_server, $db_user, $db_pwd, $dbname);
 ?>
