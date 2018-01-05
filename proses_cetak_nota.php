efb14a78abe01c2e2754a4147c14ffdb3b0c154a
rijalmyahya <rijal.myahya@gmail.com>
2018-01-05 13:45:59 +0700

    data logjual masuk 2 kali

--- proses_cetak_nota.php
+++ proses_cetak_nota.php
@@ -1,16 +1,22 @@
 <?php
+session_start();
 require("phpfpdf/fpdf.php");
 include 'koneksi.php';
+date_default_timezone_set('Asia/Jakarta');
+
 $bayar 		= $_POST['bayar'];
 $hargat		= $_POST['subtotal'];
 $kembali	= $_POST['kembali'];
+$kasir 		= $_SESSION['id_kasir'];
+$tgl		= date("Y-m-d H:i:s");
 
 $select2 = mysql_query("select * from temp");
 while ($var2 = mysql_fetch_array($select2)) {
 	$id     = $var2['id_barang'];
 	$nama   = $var2['nama_barang'];
 	$jumlah = $var2['jumlah_barang'];
-
+	$harga  = $var2['harga_barang'];
+	$sub_total = $var2['sub_total'];
 	$select = mysql_query("select * from barang where id_barang='$id' or nama_barang='$nama'");
 	while($var1 = mysql_fetch_array($select)){
 		$id2         = $var1['id_barang'];
@@ -19,8 +25,11 @@ while ($var2 = mysql_fetch_array($select2)) {
 		$sisa = $jumlah_awal-$jumlah;
 		$query_update=mysql_query("update barang set jumlah_barang='$sisa' where id_barang='$id2' or nama_barang='$nama2'");
 	}
+	$query_update_logjual= mysql_query("insert into logjual(id_barang, nama_barang, harga_barang, jumlah_barang, total_harga, id_kasir, tanggal) values ('$id','$nama','$harga','$jumlah','$sub_total','$kasir','$tgl')");	
+
 }
 
+	
 $pdf = new FPDF('l','mm','A4');
 $pdf->Addpage();
 $pdf->SetFont('Arial','B','16');
@@ -38,13 +47,14 @@ $pdf->Cell(27,6,'JUMLAH',1,0);
 $pdf->Cell(25,6,'HARGA',1,1);
  
 $pdf->SetFont('Arial','',10);
- 
+$no=1;
 $nota = mysql_query("select * from temp");
 while ($row = mysql_fetch_array($nota)){
-    $pdf->Cell(20,6,$row['no'],1,0);
+    $pdf->Cell(20,6,$no,1,0);
     $pdf->Cell(85,6,$row['nama_barang'],1,0);
     $pdf->Cell(27,6,$row['jumlah_barang'],1,0);
     $pdf->Cell(25,6,$row['sub_total'],1,1); 
+    $no++;
 }
 $pdf->Cell(20,6,'Total Harga        :' .$hargat,0,1);
 $pdf->Cell(20,6,'Uang Yang Di Bayar :' .$bayar,0,1);
